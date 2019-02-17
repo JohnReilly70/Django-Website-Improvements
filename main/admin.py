181ba0df70acf491127a3532f13900e863147491
John Reilly <johnreilly1989@hotmail.co.uk>
2019-02-17 14:48:34 +0000

    Initing Django Website Files

---
+++ main/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Tutorial, TutorialSeries, TutorialCategory
+from tinymce.widgets import TinyMCE
+from django.db import models
+
+
+# Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+                ("Title", {"fields" : ["tutorial_title"]}),
+                ("Series", {"fields": ["tutorial_series"]}),
+                ("URL", {"fields": ["tutorial_slug"]}),
+                ("Date", {"fields": ["tutorial_published"]}),
+                ("Content", {"fields" : ["tutorial_content"]})
+                 ]
+
+
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
+
+
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
+admin.site.register(Tutorial, TutorialAdmin)
